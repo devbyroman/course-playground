88f7dba38d6f62e17920f0370c47e6371947596f
Roman <roman.martyniakdev@gmail.com>
2025-08-25 17:01:14 +0300

    complete js-introduction tasks

---
+++ intro-practice.js
@@ -0,0 +1,32 @@
+// Task 1
+const language = "JavaScript"
+let year = 1995
+console.log(`${language} was created in ${year}`)
+
+// Task 2
+console.log(typeof 42)
+console.log(typeof "42")
+console.log(typeof true)
+console.log(typeof null) // object
+console.log(typeof undefined)
+
+// Task 3
+let x = 9
+const test = x >= 10 && x <= 20
+const test1 = x >= 9 && x <= 20
+const test2 = x >= 15 && x <= 20
+const test3 = x >= 20 && x <= 21
+console.log(test)
+console.log(test1)
+console.log(test2)
+console.log(test3)
+
+// Task 4
+const ffalse = 5 < 6
+const ttrue = 4 > 6
+const sstring = "string"
+const nnum = 123
+console.log(typeof ffalse)
+console.log(typeof ttrue)
+console.log(typeof nnum)
+console.log(typeof sstring)
---
+++ intro-warmup.js
@@ -0,0 +1,23 @@
+// Task 1
+const age = 18 //number
+const name = "Roman" // string
+const isAdult = true // boolean
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isAdult)
+
+//Task 2
+const smt = 2 + 3 * 4 // 14
+const nmt = (2+3) * 4 // 20
+
+//Task 3
+const str1 = "Lux"
+const str2 = "Wash"
+console.log(`${str1}${str2}`.length)
+
+//Task 4
+const num1 = 5
+const num2 = "5"
+console.log(num1 == num2) // true
+console.log(num1 === num2) // false
