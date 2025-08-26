e057b4b75e6cadc5056fcdbbbf5a49a48733c825
Roman <roman.martyniakdev@gmail.com>
2025-08-26 16:10:17 +0300

    complete strings tasks

--- warmups/jsb/numbers/numbers.js
+++ warmups/jsb/numbers/numbers.js
@@ -1,12 +1,12 @@
-// //Task 1
-// console.log(2 + 3 * 4)//16
-// console.log((2 + 3) * 4)//20
+//Task 1
+console.log(2 + 3 * 4)//16
+console.log((2 + 3) * 4)//20
 
 //Task 2
-// console.log(typeof "2" === "string" )//"2"
-// console.log(typeof 2 === "number" )//2
-// console.log(typeof true === "boolean" )//true
-// console.log(typeof false === "boolean")//false
+console.log(typeof "2" === "string" )//"2"
+console.log(typeof 2 === "number" )//2
+console.log(typeof true === "boolean" )//true
+console.log(typeof false === "boolean")//false
 
 //Difference between value and type
 //A value is a specific piece of data in memory (e.g., 2 or “2”), while a type defines the permissible operations and semantics (arithmetic for number, concatenation for string).
